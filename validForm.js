444ec64557aa7ad75d4e9c7fba7301ba2316e726
OctaviaRobinson <55119804+OctaviaRobinson@users.noreply.github.com>
2019-09-24 20:58:53 -0400

    Add files via upload

---
+++ validForm.js
@@ -0,0 +1,19 @@
+function validForm() 
+{
+  if ( inputName == "") {
+    alert("Name must be filled out");
+  else if ( inputEmail == "")
+	  alert("Email must be filled out");
+  else if (inputPhone == "")
+	  alert("Phone Number must be filled out");
+  else if (formSelect == "")
+	   alert("Reason for Inquiry is required");
+  else if (formTextArea == "")
+	  alert("This Field is required");
+  else if (formRadio = "")
+	  alert("This Field is required");
+	else
+	alert("Data Submitted");
+	
+  }
+}
